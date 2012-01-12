c1686ed9d846c47a44deca7f2ec82a7b963496bb
moore.xu@gmail.com <moore.xu@gmail.com@fe09b3dc-0e24-11de-9036-69e38a880166>
2012-01-12 14:43:23 +0000

    0.52.0 fix 1

--- trunk/bas/server.hpp
+++ trunk/bas/server.hpp
@@ -46,21 +46,14 @@ public:
       std::size_t work_pool_high_watermark = BAS_IO_SERVICE_POOL_HIGH_WATERMARK,
       std::size_t work_pool_thread_load = BAS_IO_SERVICE_POOL_THREAD_LOAD)
     : service_handler_pool_(service_handler_pool),
-    	acceptor_service_pool_(io_pool_size),
+    	acceptor_service_pool_(1),
       io_service_pool_(io_pool_size),
       work_service_pool_(work_pool_init_size, work_pool_high_watermark, work_pool_thread_load),
-     	acceptors_(),
+      acceptor_(acceptor_service_pool_.get_io_service()),
       endpoint_(boost::asio::ip::address::from_string(address), port),
       started_(false)
   {
     BOOST_ASSERT(service_handler_pool != 0);
-
-    // Create acceptor pool.
-    for (std::size_t i = 0; i < io_pool_size; ++i)
-    {
-      acceptor_ptr acceptor(new boost::asio::ip::tcp::acceptor(acceptor_service_pool_.get_io_service()));
-      acceptors_.push_back(acceptor);
-    }
 
     // Create preallocated handlers of the pool.
     service_handler_pool->init();
@@ -77,11 +70,6 @@ public:
 
     // Destroy service_handler pool.
     service_handler_pool_.reset();
-
-    // Destroy acceptor pool.
-    for (std::size_t i = 0; i < acceptors_.size(); ++i)
-      acceptors_[i].reset();
-    acceptors_.clear();
   }
 
   /// Run the server's io_service loop and stop with gracefully mode.
@@ -96,18 +84,14 @@ public:
     if (started_)
       return;
 
-    // Open acceptor pool.
-    for (std::size_t i = 0; i < acceptors_.size(); ++i)
-    {
-      // Open the acceptor with the option to reuse the address (i.e. SO_REUSEADDR).
-      acceptors_[i]->open(endpoint_.protocol());
-      acceptors_[i]->set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
-      acceptors_[i]->bind(endpoint_);
-      acceptors_[i]->listen();
+    // Open the acceptor with the option to reuse the address (i.e. SO_REUSEADDR).
+    acceptor_.open(endpoint_.protocol());
+    acceptor_.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+    acceptor_.bind(endpoint_);
+    acceptor_.listen();
   
-      // Accept new connection.
-      accept_one(acceptors_[i]);
-    }
+    // Accept new connection.
+    accept_one();
 
     // Start work_service_pool with nonblock to perform synchronous works.
     work_service_pool_.start();
@@ -151,41 +135,34 @@ public:
     if (!started_)
       return;
 
-    // Close acceptor pool.
-    for (std::size_t i = 0; i < acceptors_.size(); ++i)
-    {
-      // Close the acceptor in the same thread.
-      acceptors_[i]->get_io_service().dispatch(boost::bind(&boost::asio::ip::tcp::acceptor::close,
-        acceptors_[i]));
-    }
+    // Close the acceptor in the same thread.
+    acceptor_.get_io_service().dispatch(boost::bind(&boost::asio::ip::tcp::acceptor::close,
+        &acceptor_));
 
     // Stop accept_service_pool from block.
     acceptor_service_pool_.stop();
   }
 
 private:
-  typedef boost::shared_ptr<boost::asio::ip::tcp::acceptor> acceptor_ptr;
 
   /// Start to accept one connection.
-  void accept_one(acceptor_ptr acceptor)
+  void accept_one()
   {
     // Get new handler for accept.
     service_handler_ptr handler = service_handler_pool_->get_service_handler(io_service_pool_.get_io_service(),
         work_service_pool_.get_io_service(service_handler_pool_->get_load()));
 
     // Use new handler to accept, and bind with acceptor's io_service.
-    acceptor->async_accept(handler->socket().lowest_layer(),
+    acceptor_.async_accept(handler->socket().lowest_layer(),
         boost::bind(&server::handle_accept,
             this,
             boost::asio::placeholders::error,
-            handler,
-            acceptor));
+            handler));
   }
 
   /// Handle completion of an asynchronous accept operation.
   void handle_accept(const boost::system::error_code& e,
-      service_handler_ptr handler,
-      acceptor_ptr acceptor)
+      service_handler_ptr handler)
   {
     if (!e)
     {
@@ -193,7 +170,7 @@ private:
       handler->start();
 
       // Accept new connection.
-      accept_one(acceptor);
+      accept_one();
     }
     else
       handler->close(e);
@@ -212,8 +189,8 @@ private:
   /// The pool of io_service objects used to perform synchronous works.
   io_service_pool work_service_pool_;
 
-  /// The pool of acceptor used to listen for incoming connections.
-  std::vector<acceptor_ptr> acceptors_;
+  /// The acceptor used to listen for incoming connections.
+  boost::asio::ip::tcp::acceptor acceptor_;
 
   /// The server endpoint.
   boost::asio::ip::tcp::endpoint endpoint_;
