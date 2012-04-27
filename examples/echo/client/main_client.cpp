31f540bcfe2347a7665399ddac3e4f8a1fb6317b
moore.xu <moore.xu@gmail.com>
2012-04-27 05:47:04 +0000

    0.55.0 update echo_client example for real useful:)

--- examples/echo/client/main_client.cpp
+++ examples/echo/client/main_client.cpp
@@ -23,13 +23,13 @@ int main(int argc, char* argv[])
   try
   {
     // Check command line arguments.
-    if (argc != 11)
+    if (argc != 14)
     {
-      std::cerr << "Usage: echo_client <address> <port> <io_pool_size> <work_pool_init_size> <work_pool_high_watermark> <preallocated_handler_number> <data_buffer_size> <session_timeout> <io_timeout> <connection_number>\n";
+      std::cerr << "Usage: echo_client <address> <port> <io_pool> <work_pool_init> <work_pool_high> <handler_pool_init> <data_buffer_size> <session_timeout> <io_timeout> <pause_seconds> <connection_number> <wait_seconds> <test_times>\n";
       std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    echo_client 127.0.0.1 1000 4 4 16 100 64 30 0 100\n";
+      std::cerr << "    echo_client 127.0.0.1 1000 4 4 16 100 64 30 0 3 1000 10 10\n";
       std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    echo_client 0::0 1000 4 4 16 100 64 30 0 100\n";
+      std::cerr << "    echo_client 0::0 1000 4 4 16 100 64 30 0 3 1000 10 10\n";
       return 1;
     }
 
@@ -42,22 +42,30 @@ int main(int argc, char* argv[])
     std::size_t read_buffer_size = boost::lexical_cast<std::size_t>(argv[7]);
     unsigned int session_timeout = boost::lexical_cast<unsigned int>(argv[8]);
     unsigned int io_timeout = boost::lexical_cast<unsigned int>(argv[9]);
-    std::size_t connection_number = boost::lexical_cast<std::size_t>(argv[10]);
+    unsigned int pause_seconds = boost::lexical_cast<unsigned int >(argv[10]);
+    std::size_t connection_number = boost::lexical_cast<std::size_t>(argv[11]);
+    unsigned int wait_seconds = boost::lexical_cast<unsigned int >(argv[12]);
+    unsigned int test_times = boost::lexical_cast<unsigned int >(argv[13]);
 
     typedef bas::service_handler_pool<echo::client_work, echo::client_work_allocator> client_handler_pool;
+    echo::error_count counter;
 
-    echo::connections client(argv[1],
+    echo::connections client(new client_handler_pool(new echo::client_work_allocator(&counter, pause_seconds),
+                                 preallocated_handler_number,
+                                 read_buffer_size,
+                                 0,
+                                 session_timeout,
+                                 io_timeout),
+        &counter,
+        argv[1],
         port,
         io_pool_size,
         work_pool_init_size,
         work_pool_high_watermark,
+        pause_seconds,
         connection_number,
-        new client_handler_pool(new echo::client_work_allocator(),
-            preallocated_handler_number,
-            read_buffer_size,
-            0,
-            session_timeout,
-            io_timeout));
+        wait_seconds,
+        test_times);
 
     // Run the client until stopped.
     client.run();
