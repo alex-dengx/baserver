934c5bb3c78ca3e0f834e0d89e92815dfef70226
moore.xu@gmail.com <moore.xu@gmail.com@fe09b3dc-0e24-11de-9036-69e38a880166>
2012-06-01 01:51:16 +0000

    0.55.2 client example update.

--- trunk/examples/echo/client/main_client.cpp
+++ trunk/examples/echo/client/main_client.cpp
@@ -23,13 +23,13 @@ int main(int argc, char* argv[])
   try
   {
     // Check command line arguments.
-    if (argc != 14)
+    if (argc != 13)
     {
-      std::cerr << "Usage: echo_client <address> <port> <io_pool> <work_pool_init> <work_pool_high> <handler_pool_init> <data_buffer_size> <session_timeout> <io_timeout> <pause_seconds> <connection_number> <wait_seconds> <test_times>\n";
+      std::cerr << "Usage: echo_client <address> <port> <io_pool_size> <work_pool_size> <handler_pool_init> <data_buffer_size> <session_timeout> <io_timeout> <pause_seconds> <connection_number> <wait_seconds> <test_times>\n";
       std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    echo_client 127.0.0.1 1000 4 4 16 100 64 30 0 3 1000 10 10\n";
+      std::cerr << "    echo_client 127.0.0.1 1000 4 16 100 64 30 0 3 1000 10 10\n";
       std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    echo_client 0::0 1000 4 4 16 100 64 30 0 3 1000 10 10\n";
+      std::cerr << "    echo_client 0::0 1000 4 16 100 64 30 0 3 1000 10 10\n";
       return 1;
     }
 
@@ -38,31 +38,29 @@ int main(int argc, char* argv[])
     // Initialise server.
     unsigned short port = boost::lexical_cast<unsigned short>(argv[2]);
     std::size_t io_pool_size = boost::lexical_cast<std::size_t>(argv[3]);
-    std::size_t work_pool_init_size = boost::lexical_cast<std::size_t>(argv[4]);
-    std::size_t work_pool_high_watermark = boost::lexical_cast<std::size_t>(argv[5]);
-    std::size_t preallocated_handler_number = boost::lexical_cast<std::size_t>(argv[6]);
-    std::size_t read_buffer_size = boost::lexical_cast<std::size_t>(argv[7]);
-    unsigned int session_timeout = boost::lexical_cast<unsigned int>(argv[8]);
-    unsigned int io_timeout = boost::lexical_cast<unsigned int>(argv[9]);
-    unsigned int pause_seconds = boost::lexical_cast<unsigned int >(argv[10]);
-    std::size_t connection_number = boost::lexical_cast<std::size_t>(argv[11]);
-    unsigned int wait_seconds = boost::lexical_cast<unsigned int >(argv[12]);
-    unsigned int test_times = boost::lexical_cast<unsigned int >(argv[13]);
+    std::size_t work_pool_size = boost::lexical_cast<std::size_t>(argv[4]);
+    std::size_t preallocated_handler_number = boost::lexical_cast<std::size_t>(argv[5]);
+    std::size_t read_buffer_size = boost::lexical_cast<std::size_t>(argv[6]);
+    unsigned int session_timeout = boost::lexical_cast<unsigned int>(argv[7]);
+    unsigned int io_timeout = boost::lexical_cast<unsigned int>(argv[8]);
+    unsigned int pause_seconds = boost::lexical_cast<unsigned int >(argv[9]);
+    std::size_t connection_number = boost::lexical_cast<std::size_t>(argv[10]);
+    unsigned int wait_seconds = boost::lexical_cast<unsigned int >(argv[11]);
+    unsigned int test_times = boost::lexical_cast<unsigned int >(argv[12]);
 
     typedef bas::service_handler_pool<echo::client_work, echo::client_work_allocator> client_handler_pool;
     echo::error_count counter;
 
-    echo::connections client(new client_handler_pool(new echo::client_work_allocator(&counter, pause_seconds),
+    echo::connections client(new client_handler_pool(new echo::client_work_allocator(counter, pause_seconds),
                                  preallocated_handler_number,
                                  read_buffer_size,
                                  0,
                                  session_timeout,
                                  io_timeout),
-        &counter,
+        counter,
         tcp::endpoint(address::from_string(argv[1]), port),
         io_pool_size,
-        work_pool_init_size,
-        work_pool_high_watermark,
+        work_pool_size,
         pause_seconds,
         connection_number,
         wait_seconds,
